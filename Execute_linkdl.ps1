a52c9fdade2798642c3df46f2a050746ec058817
daksh2k <daksh2k@gmail.com>
2021-03-31 01:03:48 +0530

    Added drive link opening support

--- Execute_linkdl.ps1
+++
@@ -1,6 +0,0 @@
-# cd D:/College/Projects/zippyshare-dl/Links
-D:/College/Projects/zippyshare-dl/env/Scripts/activate
-chrome.exe -remote-debugging-port=4000 --user-data-dir="D:/College/Projects/zippyshare-dl/Selenium/Chrome_Test_Profile"
-start "C:\Program Files (x86)\Internet Download Manager\IDMan.exe"
-py D:/College/Projects/zippyshare-dl/linkdl.py | tee -a D:/College/Projects/zippyshare-dl/Logs/log_linkdl.txt
-deactivate
--- Execute_testaltdl.ps1
+++
@@ -1,5 +0,0 @@
-D:/College/Projects/zippyshare-dl/env/Scripts/activate
-# chrome.exe -remote-debugging-port=4000 --user-data-dir="D:/College/Projects/zippyshare-dl/Selenium/Chrome_Test_Profile"
-py D:/College/Projects/zippyshare-dl/testaltdl.py | tee -a D:/College/Projects/zippyshare-dl/Logs/log_testaltdl.txt
-subl D:/College/Projects/zippyshare-dl/Logs/log_testaltdl.txt
-deactivate
