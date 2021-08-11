da1c98dc58c7e1c63fb270179f562c935c02f347
daksh2k <daksh2k@gmail.com>
2021-08-12 04:02:06 +0530

    Update cdriver

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ DIR_CHECK          =  1      # If you Want to Auto Add files dlc and txt files f
 SKIP_DUP           =  0      # Skip Duplicate Check For Files 
 SKIP_DEAD          =  3      # Skip file after certain number of Links are found Dead.Put 0 for not skipping.
 RETRY_COUNT        =  2      # Retry If unable to Open Link Min value=1 
-DIRS_TO_CHECK      =  ['.','./Links',os.path.expanduser("~")+"\\Downloads"]                   # Following Dirs to automatically check for dlc and text files.
-COMMON_NAMES       =  ["req.*\.txt",".*dl_links\.txt","temp_.*",".*\d{2}_\d{2}_\d{4}\.txt"]   # Common Names to Ignore from Directory, Supports Regex
+DIRS_TO_CHECK      =  ('.','./Links',os.path.expanduser("~")+"\\Downloads")                   # Following Dirs to automatically check for dlc and text files.
+COMMON_NAMES       =  ("req.*\.txt",".*dl_links\.txt","temp_.*",".*\d{2}_\d{2}_\d{4}\.txt")   # Common Names to Ignore from Directory, Supports Regex
